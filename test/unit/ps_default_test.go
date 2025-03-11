9c24f55fb765d3453fd5cd14ef464b8ddec4145b
Jiaxi Geng <jess@Jiaxis-MacBook-Pro.local>
2025-03-11 18:42:19 +0000

    Update terratest case

--- test/unit/ps_default_test.go
+++ test/unit/ps_default_test.go
@@ -18,13 +18,13 @@ func TestTerraformParallelStoreDefault(t *testing.T) {
 
 	instanceNameDefault := terraform.Output(t, terraformOptions, "instance_name")
 	instanceIDDefault := terraform.Output(t, terraformOptions, "instance_id")
-	instanceLabelDefault := terraform.Output(t, terraformOptions, "labels")
+	instanceLabelDefault := terraform.Output(t, terraformOptions, "tags")
 	instanceRegion := terraform.Output(t, terraformOptions, "region")
 	instanceCapacity := terraform.Output(t, terraformOptions, "capacity_gb")
 
 	assert.NotEmpty(t, instanceNameDefault, "ParallelStore instance name should not be empty")
 	assert.NotEmpty(t, instanceIDDefault, "ParallelStore ID should not be empty")
 	assert.NotEmpty(t, instanceLabelDefault, "ParallelStore label should not be empty")
-	assert.Equal(t, "us-central1-a", instanceRegion, "Region should be us-central1")
-	assert.Equal(t, "27000", instanceCapacity, "Capacity should be 27000GB")
+	assert.Equal(t, "[us-central1-a]", instanceRegion, "Region should be us-central1")
+	assert.Equal(t, "[27000]", instanceCapacity, "Capacity should be 27000GB")
 }
